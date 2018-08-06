022dfe52393bfb92de2b95b9b6f4f82734534828
Catherine Bronzo <cbronzo@mac.com>
2018-08-06 20:51:48 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -8,15 +8,14 @@ def initialize(name, breed, age)
   @name = name
   @breed = breed
   @age = age
-end
-
-def self
   @@all << self
 end
-  
-def all
+
+def self.all
   @@all
-end  
+end
+  
+
   
 
 end
